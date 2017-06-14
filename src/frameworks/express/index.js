52ec09781b53df045ed6bde69532716cabe39d49
Faraz <github@services.farazsyed.com>
2017-06-13 22:19:46 -0600

    Simplify Express framework connector
    Remove redundant `express.Router()`

--- src/frameworks/express/index.js
+++ src/frameworks/express/index.js
@@ -1,15 +1,9 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-
-import express from 'express';
-
 module.exports = function(CoreFramework) {
   return class Express extends CoreFramework {
     app() {
-      return express.Router().use(this.expressify());
+      return this.expressify();
     }
   };
 };
