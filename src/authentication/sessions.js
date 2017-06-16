bb85a90b6941c8f6d20b2cc6e15359d7daf9b461
Faraz <github@services.farazsyed.com>
2017-06-15 21:58:53 -0600

    Add missing file from last commit

--- src/authentication/sessions.js
+++ src/authentication/sessions.js
@@ -9,46 +9,45 @@ export default {
  */
 
 import SequelizeStore from 'koa-generic-session-sequelize';
+import convert from 'koa-convert';
 import session from 'koa-generic-session';
 
 /**
  * Prepares session middleware and methods without attaching to the stack
  *
- * @param {Object} router
  * @param {Object} settings
  * @param {Object} database
  * @param {Object} [store] Store for `koa-generic-sessions`. Uses the database if a store is not provided
  * @return {Object}
  */
-function setup(router, settings, database, store) {
+function setup(settings, database, store) {
+  return {
+    // session middleware
+    session: convert(session({
+      key: 'identityDesk.sid',
+      store: store || new SequelizeStore(database),
+    })),
+    sessionMethods: function(ctx, next) {
+      // set the secret keys for Keygrip
+      ctx.app.keys = settings.session.keys;
 
-  // attach pre-authenticator middleware
-  router.use(function(ctx, next) {
-    // set the secret keys for Keygrip
-    ctx.app.keys = settings.session.keys;
+      // attach session methods
+      ctx.identityDesk = {
+        get(key) {
+          if (ctx.session && ctx.session.identityDesk && ctx.session.identityDesk[key] !== undefined) {
+            return ctx.session.identityDesk[key];
+          } else {
+            return undefined;
+          }
+        },
+        set(values) {
+          ctx.session = ctx.session || { identityDesk: { values } };
+          ctx.session.identityDesk = ctx.session.identityDesk || { values };
+          Object.assign(ctx.session.identityDesk, values);
+        },
+      };
 
-    // attach session methods
-    ctx.identityDesk = {
-      get(key) {
-        if (ctx.session && ctx.session.identityDesk && ctx.session.identityDesk[key] !== undefined) {
-          return ctx.session.identityDesk[key];
-        } else {
-          return undefined;
-        }
-      },
-      set(values) {
-        ctx.session = ctx.session || { identityDesk: { values } };
-        ctx.session.identityDesk = ctx.session.identityDesk || { values };
-        Object.assign(ctx.session.identityDesk, values);
-      },
-    };
-
-    next();
-  });
-
-  // return the session middleware for later use
-  return session({
-    key: 'identityDesk.sid',
-    store: store || new SequelizeStore(database),
-  });
+      next();
+    },
+  };
 }
