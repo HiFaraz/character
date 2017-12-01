fa6d9a9d4cc71165755e12711c53f61e504f4a7a
Faraz Syed <github@services.farazsyed.com>
2017-11-30 22:58:18 -0700

    Add more test for Plugin base class API

--- src/plugin.spec.js
+++ src/plugin.spec.js
@@ -4,7 +4,23 @@ import Plugin from './plugin';
 import assert from 'assert';
 
 describe('Plugin class', () => {
-  it('define should throw error (called by constructor)', () => {
+  it('defaults should return an object', () => {
+    assert(typeof Plugin.defaults() === 'object');
+  });
+
+  it('models should return an object', () => {
+    assert(typeof Plugin.models() === 'object');
+  });
+
+  it('name should return an empty string', () => {
+    assert(Plugin.name() === '');
+  });
+
+  it('validateConfig should return true', () => {
+    assert(Plugin.validateConfig() === true);
+  });
+
+  it('#define should throw error (called by constructor)', () => {
     try {
       new Plugin();
       throw new Error();
