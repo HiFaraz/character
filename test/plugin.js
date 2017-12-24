a316612c40248f99ec882a9092bb245d88c8da2d
Faraz Syed <hello@farazsyed.com>
2017-12-24 00:58:22 -0500

    Attach plugin models to plugin context, and use plugin constructor name

--- test/plugin.js
+++ test/plugin.js
@@ -3,6 +3,10 @@
 const Plugin = require('../lib/plugin');
 const assert = require('assert');
 
+const character = {
+  database: {},
+};
+
 describe('Plugin class', () => {
   it('defaults should return an object', () => {
     assert(typeof Plugin.defaults() === 'object');
@@ -12,20 +16,23 @@ describe('Plugin class', () => {
     assert(typeof Plugin.models() === 'object');
   });
 
-  it('name should return an empty string', () => {
-    assert(Plugin.name() === '');
+  it('name should return the class name', () => {
+    class MyPlugin extends Plugin {
+      define() {} // override define() to avoid error
+    }
+    assert(MyPlugin.name === 'MyPlugin');
   });
 
   it('validateConfig should return true', () => {
     class MyPlugin extends Plugin {
       define() {} // override define() to avoid error
     }
-    assert(new MyPlugin().validateConfig() === true);
+    assert(new MyPlugin({}, {}, character).validateConfig() === true);
   });
 
   it('#define should throw error (called by constructor)', () => {
     try {
-      new Plugin();
+      new Plugin({}, {}, character);
       throw new Error();
     } catch (error) {
       assert(error.message === 'Plugin#define must be overridden by subclass');
